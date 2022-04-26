8093b39b120eed18895d5176481663137d88cab4
t_shinnosuke <97178451+takayanagishinnosuke@users.noreply.github.com>
2022-04-27 00:39:29 +0900

    0427

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import random
-
+from flask_cors import CORS
 from requests import request
 
 keyval = 0
@@ -12,6 +12,7 @@ def function():
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=["GET"])
 def test():
@@ -20,4 +21,3 @@ def test():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
