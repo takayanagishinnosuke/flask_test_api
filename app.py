7095029b0ed2a10ca13fd4b315c349433b25d3c1
t_shinnosuke <97178451+takayanagishinnosuke@users.noreply.github.com>
2022-04-26 23:49:13 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import random
+
+from requests import request
+
+keyval = 0
+
+
+def function():
+  global keyval
+  keyval = random.randint(0,1)
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def test():
+    function()
+    return jsonify({'key': keyval})
+
+if __name__ == "__main__":
+    app.run(debug=True)
