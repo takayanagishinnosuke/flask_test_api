60e44207761b39b283dfbe4de7fbcb69cad13f72
t_shinnosuke <97178451+takayanagishinnosuke@users.noreply.github.com>
2022-05-02 03:52:39 +0900

    0501

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, jsonify
-import random
+from crypt import methods
+from distutils.log import error
+from flask import Flask, jsonify, request, url_for
+import json
+import base64
+from io import BytesIO
 from flask_cors import CORS
-from requests import request
+import numpy as np
+import cv2
+import face_recognition
 
-keyl = ['OK', 'NG']
-
-
-def function():
-  global keyval
-  keyval = random.choice(keyl)
-
-
+img_gray =[] #np配列入れるリストを用意
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False
 CORS(app)
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=("GET", 'POST'))
 def test():
-    function()
-    key = keyval
-    return key
+  if request.method == 'POST':
+    posted = request.get_json()
+    key = posted['img'] ##imgというjsonのbodyで入ってきたら
+    
+    img_stream = base64.b64decode(key) ##keyをデコード
+    img_array = np.asarray(bytearray(img_stream), dtype=np.uint8) #np配列に
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR) #opencvで使える形式に
+    
+    global img_gray
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) ##白黒に
+    
+    #cv2.imwrite('test.jpg',img_gray) ##テストでjpg保存
+    ## recognition関数へ渡す
+    key = face_recognition.recognition(img_gray)
+    print(key) ##確認用
+
+    return jsonify({"result": key}) ##jsonにして返す
+
+
+  else:
+    return jsonify({"result": "POST失敗 エラー"})
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000)
---
+++ face_recognition.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os 
+from time import sleep
+import app
+
+##-判定関数--##
+def recognition(post):
+    key = ''
+
+    ##--カスケードファイルの指定--##
+    cascadePath = "haarcascade_frontalface_default.xml"
+    ##--カスケードモデルの定義--##
+    faceCascade = cv2.CascadeClassifier(cascadePath);
+
+    # img = cv2.imread('test.jpg') #テスト用
+    
+    img = post
+
+    faces = faceCascade.detectMultiScale( 
+        img,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+    )
+    ##--facesがあるかないか判定する--##
+    if (len(faces) > 0):
+        key = 'Danger'
+    else:
+        key = "NoProblem"
+    
+    return key
+
